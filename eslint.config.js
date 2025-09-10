60fed06fe8cace3ae18c33739c23cc86cdea1daa
Giovanni Barroso <giovanni.baralv@gmail.com>
2025-09-10 17:09:42 +0200

    deploy 3#

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+// eslint.config.js
+export default [
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        window: "readonly",
+        document: "readonly"
+      }
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      "no-console": "off"
+    }
+  }
+];
